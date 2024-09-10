dbeca0db21bf5bfa51f506595d6f6f61ef51accc
Marco Ocanto <mocanto@cnti.gob.ve>
2024-09-10 15:13:26 -0400

    shell_django

--- my_first_app/models.py
+++ my_first_app/models.py
@@ -2,3 +2,25 @@ from django.db import models
 
 class Carro(models.Model):
     title = models.TextField(max_length=255)
+    years= models.CharField(max_length=8,null=True)
+    colors=models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return f" {self.title} -{self.years}"
+
+class Conductores(models.Model):
+    name = models.TextField(max_length=255)
+    years= models.CharField(max_length=8,null=True)
+    sex=models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return f" {self.name} -{self.years}--{self.sex}"
+    
+class Licen(models.Model):
+    title = models.TextField(max_length=200)
+    asig = models.ForeignKey(Conductores, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.title}---{self.asig}"
+    
